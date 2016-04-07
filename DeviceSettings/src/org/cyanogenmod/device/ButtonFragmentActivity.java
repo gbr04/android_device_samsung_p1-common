469077659caa67f07bd2aa7c3671feb07e7eed21
feroxx <frhatfrat@gmail.com>
2016-04-07 14:11:15 +0300

    Update ButtonFragmentActivity.java

--- DeviceSettings/src/org/cyanogenmod/device/ButtonFragmentActivity.java
+++ DeviceSettings/src/org/cyanogenmod/device/ButtonFragmentActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.cyanogenmod.device;
+package com.cyanogenmod.settings.device;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -28,7 +28,7 @@ import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.util.Log;
 
-import org.cyanogenmod.device.R;
+import com.cyanogenmod.settings.device.R;
 
 public class ButtonFragmentActivity extends PreferenceFragment {
 
